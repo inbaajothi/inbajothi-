952b6012def7eebf3cec3dce5baaef1738e2c126
inbaajothi <114497100+inbaajothi@users.noreply.github.com>
2022-10-03 07:39:41 -0700

    Add files via upload

--- main.c
+++ main.c
@@ -3,28 +3,14 @@
 
 int main()
 {
-    int a=5,b=5,c=10;
+    int i =1;
 
+    while (i <= 5) {
+            printf("%d\n", i);
+            ++i;
+    }
 
-
-
-    printf("(a ==b) && (c>b) is %d\n", result);
-
-    result = (a ==b) && (c<b);
-    printf("(a ==b) && (c<b) is %d\n", result);
-
-    result = (a==b) || (c<b);
-    printf("(a==b) || (c<b) is %d\n", result);
-
-    result = (a!=b) || (c<b);
-    printf("(a!=b) || (c<b) is %d\n", result);
-
-    result = !(a !=b);
-    printf("!(a !=b) is %d\n", result);
-
-    result = !(a ==b);
-    printf("!(a ==b) is %d\n", result);
 
     return 0;
-}
 
+}
