9f57b100637d93209e99b2c7bcfa66fc69889140
inbaajothi <114497100+inbaajothi@users.noreply.github.com>
2022-10-04 08:16:29 -0700

    Add files via upload

--- main.c
+++ main.c
@@ -3,18 +3,48 @@
 
 int main()
 {
-    int n, reverse = 0, remainder;
+    int low, high, number, originalNumber, rem, count = 0;
+    double result = 0.0;
+    printf("Enter two numbers(intervals); ");
+    scanf("%d %d", &low, &high);
+    printf("Armstrong numbers between %d and %d are: ", low, high);
 
-    printf("Enter an integer:");
-    scanf("%d", &n);
 
-    while (n!= 0) {
-            remainder =n  % 10;
-            reverse = reverse * 10 + remainder;
-            n/= 10;
+    if (high < low) {
+        high += low;
+        low = high - low;
+        high -= low;
     }
 
-    printf("Reversed number = %d", reverse);
+
+
+    for (number = low + 1; number < high; ++number) {
+        originalNumber = number;
+
+
+        while (originalNumber != 0) {
+            originalNumber /= 10;
+            ++count;
+        }
+
+        originalNumber = number;
+
+
+        while (originalNumber != 0) {
+            rem = originalNumber % 10;
+            result += pow(rem, count);
+            originalNumber /= 10;
+        }
+
+
+        if ((int)result == number) {
+            printf("%d ", number);
+        }
+
+
+        count = 0;
+        result = 0;
+    }
 
     return 0;
 }
