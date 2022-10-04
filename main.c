ad06e20a570ef221b4046208ecbe966d64cb7810
inbaajothi <114497100+inbaajothi@users.noreply.github.com>
2022-10-04 09:30:04 -0700

    Add files via upload

--- main.c
+++ main.c
@@ -3,48 +3,17 @@
 
 int main()
 {
-    int low, high, number, originalNumber, rem, count = 0;
-    double result = 0.0;
-    printf("Enter two numbers(intervals); ");
-    scanf("%d %d", &low, &high);
-    printf("Armstrong numbers between %d and %d are: ", low, high);
-
-
-    if (high < low) {
-        high += low;
-        low = high - low;
-        high -= low;
+    int rows,i,j,number = 1;
+    printf("Enter the number of  rows:");
+    scanf("%d", &rows);
+    for(i = 1; i <= rows; i++) {
+            for(i = 1; j <= i; ++j) {
+                printf("%d", number);
+                ++number;
+            }
+            printf("\n");
     }
-
-
-
-    for (number = low + 1; number < high; ++number) {
-        originalNumber = number;
-
-
-        while (originalNumber != 0) {
-            originalNumber /= 10;
-            ++count;
-        }
-
-        originalNumber = number;
-
-
-        while (originalNumber != 0) {
-            rem = originalNumber % 10;
-            result += pow(rem, count);
-            originalNumber /= 10;
-        }
-
-
-        if ((int)result == number) {
-            printf("%d ", number);
-        }
-
-
-        count = 0;
-        result = 0;
-    }
-
     return 0;
+
+
 }
